019bdc3b8e20967f7f5fef7390fc93584664c897
Shiyu Liu <jamessliu2020@gmail.com>
2020-09-06 18:43:00 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,65 @@
+(function(){
+  'use strict';
+  
+  angular.module('searchListApp', [])
+  .controller('searchListController', searchListController)
+  .service('searchListService', searchListService)
+  .constant('ApiBasePath', "https://davids-restaurant.herokuapp.com/menu_items.json")
+  .directive('searchList', SearchListDirective);
+
+  function SearchListDirective() {
+    var ddo = {
+      templateUrl: 'searchList.html', 
+      scope: {
+          found: '<',
+          searchTerm: '<',
+          onRemove : '&'
+      }, 
+      // controller: searchListController, 
+      // controllerAs: 'list',
+      // bindToController: false
+    };
+    return ddo;
+  }
+
+
+  searchListController.$inject = ['searchListService'];
+  function searchListController( searchListService) {
+
+    var list = this;
+    
+    list.searchTerm = "";
+    list.found = searchListService.getMatchedItems(list.searchTerm);
+
+    list.removeItem = function(index) {
+      list.found.splice(index,1);
+    }
+
+  }
+
+  searchListService.$inject = ['$http', 'ApiBasePath']; 
+  function searchListService($http, ApiBasePath) {
+    var service = this;
+
+    service.getMatchedItems = function(searchTerm) {
+      return $http({
+        method: 'GET', 
+        url: ApiBasePath
+      }).then(function (result) {
+        let items = result.data;
+        console.log(items);
+        // process result and only keep items that match
+        // var foundItems = items.filter(function(str){return str.include(searchTerm)});
+        // return processed items
+        return items;
+      }).catch(function(error){
+        console.log('Error: cannot get menu items.');
+      });
+    }
+    
+    
+  }
+  
+  })();
+
+ 
